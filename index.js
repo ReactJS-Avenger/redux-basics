67563c00ebeec43b676d5cb325047b6871e3da3c
Shubham V <Shubham.V@cerner.com>
2021-04-16 20:18:29 +0530

    middleware

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const redux= require('redux')
+const reduxLogger= require('redux-logger')
 const createStore= redux.createStore
+const combineReducer= redux.combineReducers
+const applyMiddleware= redux.applyMiddleware
+const logger= reduxLogger.createLogger()
 
 const BUY_CAKE="BUY_CAKE";
 const BUY_ICECREAM= "BUY_ICECREAM"
@@ -22,18 +26,48 @@ function buyIceCream(){
 // Reducers
 // (prevState, actions) => newState
 
-const initialState={
-    numberOfCakes: 10,
+// const initialState={
+//     numberOfCakes: 10,
+//     numberOfIceCream: 20
+// }
+
+const initialCakeState={
+    numberOfCakes: 10
+}
+
+const initialIcecreamState={
     numberOfIceCream: 20
 }
 
-const reducers=(state= initialState, action)=>{
+// const reducers=(state= initialState, action)=>{
+//     switch(action.type){
+//         case BUY_CAKE:
+//             return {
+//                 ...state,
+//                 numberOfCakes: state.numberOfCakes -1
+//             }
+//         case BUY_ICECREAM:
+//             return {
+//                 ...state,
+//                 numberOfIceCream: state.numberOfIceCream -1
+//             }
+//         default: return state
+//     }
+// }
+
+const cakeReducers=(state= initialCakeState, action)=>{
     switch(action.type){
         case BUY_CAKE:
             return {
                 ...state,
                 numberOfCakes: state.numberOfCakes -1
             }
+        default: return state
+    }
+}
+
+const iceCreamReducers=(state= initialIcecreamState, action)=>{
+    switch(action.type){
         case BUY_ICECREAM:
             return {
                 ...state,
@@ -43,12 +77,18 @@ const reducers=(state= initialState, action)=>{
     }
 }
 
+// combine reducer
+const rootReducer= combineReducer({
+    cake: cakeReducers,
+    iceCream: iceCreamReducers
+})
+
 // Store
-const store= createStore(reducers) //redux store holding the application state
+const store= createStore(rootReducer, applyMiddleware(logger)) //redux store holding the application state
 
 console.log("Initial state", store.getState()) // using getState()
 
-const unsubscribe=store.subscribe(()=> console.log("Updated state", store.getState())) // using subscribe()
+const unsubscribe=store.subscribe(()=> {}) // using subscribe()
 
 store.dispatch(buyCake()) // dispatching an action
 store.dispatch(buyCake())
